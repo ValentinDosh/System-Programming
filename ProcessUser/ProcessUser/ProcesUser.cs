18088aab71df1a575b2ae0bd73433dd27ca10a8e
Valentin <harrypotter9173656@gmail.com>
2018-02-28 02:29:35 +0300

    Console App for ProcessMenu for checking

---
+++ ProcessUser/ProcessUser/ProcesUser.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProcessUser
+{
+    class ProccesUser
+    {
+        private IOrderedEnumerable<Process> _proceces;
+
+        // текущий процесс(последний с которым ввзаимодействовали)
+        private Process _currentProcessId;
+        
+        // узнать(получить) текущий процесс
+        public Process CurrentProcess
+        {
+            get
+            {
+                return _currentProcessId;
+            }
+        }
+
+        public ProccesUser() 
+        {
+            _proceces = null;
+            _currentProcessId = null;
+        }
+
+        // список всех процессов
+        public void DisplayProcceces()
+        {
+            _proceces = Process.GetProcesses(".").Select(proc => proc).OrderBy(proc => proc.Id);
+
+            foreach (var item in _proceces)
+            {
+                Console.WriteLine("({0}) {1}", item.Id, item.ProcessName);
+            }
+        }
+
+        // выбрать процесс по Id
+        public bool IdProcess(int id)
+        {
+            if (_proceces == null)
+            {
+                _proceces = Process.GetProcesses(".").Select(proc => proc).OrderBy(proc => proc.Id);
+            }
+            try
+            {
+                foreach (var item in _proceces)
+                {
+                    if (id == item.Id)
+                    {
+                        _currentProcessId = item;
+                        return true;
+                    }
+                }
+                return false;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+        }
+
+
+
+        // запустить выбранный процесс
+        public void ProcessStart(string argumnent)
+        {
+            try
+            {
+                _currentProcessId = Process.Start(argumnent);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        public void ProcessStart(string FileName, string argumnent)
+        {
+            try
+            {
+                _currentProcessId = Process.Start(FileName, argumnent);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        public void ProcessStart(ProcessStartInfo arg)
+        {
+            try
+            {
+                _currentProcessId = Process.Start(arg);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+
+        // остановить процесс используя Id
+        public void Stop(int id)
+        {
+            if (IdProcess(id))
+            {
+                _currentProcessId.Kill();
+                _currentProcessId = null;
+            }
+
+        }
+
+        // остановить текущий процесс используя
+        public void Stop()
+        {
+            _currentProcessId.Kill();
+            _currentProcessId = null;
+        }
+
+
+        // информация по потокам(выодим инф текущего)
+        public void DisplayThreads()
+        {
+            try
+            {
+                var threads = _currentProcessId.Threads;
+                foreach (ProcessThread item in threads)
+                {
+                    Console.WriteLine("Id потока: {0}, время запуска потока: {1}", item.Id, item.StartTime);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        // информация по модулям
+        public void DisplayModules()
+        {
+            try
+            {
+                var modules = _currentProcessId.Modules;
+                foreach (ProcessModule item in modules)
+                {
+                    Console.WriteLine("Имя модуля: {0}, Путь модуля: {1}", item.ModuleName, item.FileName);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ ProcessUser/ProcessUser/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProcessUser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProccesUser process = new ProccesUser();
+            menu();
+            int result = Convert.ToInt32(Console.ReadLine());
+
+            while (result != 0)
+            {
+                switch (result)
+                {
+                    case 1:
+                        Console.Clear();
+                        menu();
+                        process.DisplayProcceces();    
+                        break;
+                    case 2:
+                        Console.Clear();
+                        menu();
+                        Console.Write("Введите Id: ");
+                        int id = Convert.ToInt32(Console.ReadLine());
+                        process.IdProcess(id);
+                        break;
+                    case 3:
+                        Console.Clear();
+                        menu();
+                        Console.Write("Введите путь к файлу: ");
+                        string path = Console.ReadLine();
+                        process.ProcessStart(path);
+                        break;
+                    case 4:
+                        Console.Clear();
+                        menu();
+                        Console.Write("Введите Id: ");
+                        int id2 = Convert.ToInt32(Console.ReadLine());
+                        process.Stop(id2);
+                        break;
+                    case 5:
+                        Console.Clear();
+                        menu();
+                        process.DisplayThreads();
+                        break;
+                    case 6:
+                        Console.Clear();
+                        menu();
+                        process.DisplayModules();
+                        break;
+                    default:
+                        break;
+                }
+                result = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+
+        static void menu()
+        {
+            Console.WriteLine("Выберите операцию:\n");
+            Console.WriteLine("1 - Список всех процессов");
+            Console.WriteLine("2 - Выбор процесса по PID");
+            Console.WriteLine("3 - Запустить процесс");
+            Console.WriteLine("4 - Остановить процесс");
+            Console.WriteLine("5 - Информация о потоках выбранного процесса");
+            Console.WriteLine("6 - Информация о модулях выбранного процесса");
+            Console.WriteLine("0 - Выход");
+        }
+    }
+}
