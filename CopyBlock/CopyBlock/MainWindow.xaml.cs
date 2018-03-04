a761f51445d5ce438f5d57c1ba30057c26c4d8c9
Valentin <harrypotter9173656@gmail.com>
2018-03-05 02:04:03 +0300

    Copy file blocks 4096KB for checking

---
+++ CopyBlock/CopyBlock/MainWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CopyBlock
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        MyThreadRead _read;
+        MyThreadWrite _write;
+        AutoResetEvent _autoReset;
+        public static double barLevel;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            _autoReset = new AutoResetEvent(false);
+        }
+
+
+        // get path from(button)
+        private void Button_Click_FromFilePath(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openF = new OpenFileDialog();
+            if (openF.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                TextBoxFrom.Text = openF.FileName;
+                _read = new MyThreadRead(_autoReset, openF.FileName);
+            }
+        }
+
+        // get path to(button)
+        private void Button_Click_ToFilePath(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveF = new SaveFileDialog();
+            if (saveF.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                TextBoxTo.Text = saveF.FileName;
+                _write = new MyThreadWrite(_autoReset, _read, saveF.FileName);
+            }
+        }
+
+
+        private void Button_Click_Copy(object sender, RoutedEventArgs e)
+        {
+            if (TextBoxTo.Text == "" || TextBoxFrom.Text == "")
+            {
+                System.Windows.Forms.MessageBox.Show("Введите путь");
+            }
+            else
+            {
+                _write.Start();
+               
+                new Thread(ProgressBarState).Start();
+
+            }
+
+        }
+
+        private void ProgressBarState()
+        {
+            while (!(_write.FileLength < _read.Position)){
+                Dispatcher.Invoke(() => ProgressBarCopy.Value = barLevel);
+            }
+        }
+    }
+}
---
+++ CopyBlock/CopyBlock/MyThreadWrite.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Forms;
+//using CopyBlock.Properties;
+
+namespace CopyBlock
+{
+    class MyThreadWrite
+    {
+        private Thread _thrd;
+        private MyThreadRead _read;
+        private string toFileName;
+        private double _fileLength;
+        private readonly AutoResetEvent _autoReset;
+
+        public MyThreadWrite(AutoResetEvent autoReset, MyThreadRead read, string path)
+        {
+            _read = read;
+            toFileName = path;
+            _autoReset = autoReset;
+        }
+
+        public double FileLength
+        {
+            get
+            {
+                return _fileLength;
+            }
+        }
+
+
+        public void Start()
+        {
+            _thrd = new Thread(Write);
+            _thrd.Start();
+        }
+
+        public void Write()
+        {
+                // get length all file
+            using (FileStream read = File.OpenRead(_read.FromFileName))
+            {
+                _fileLength = read.Length;
+            }
+
+            while (_read.Position < _fileLength)
+            {
+                // в главном окне создать этот обьект
+
+                // started reading
+                _read.Start();
+               
+                // wait reading thread 
+                _autoReset.WaitOne();
+
+                // and now writing  to file (but now we need read again...)
+
+                // right writeing last block
+                if (_fileLength < _read.Position)
+                {
+                    using (FileStream write = new FileStream(toFileName, FileMode.Append))
+                    {
+                        write.Write(_read.Array, 0, (int)(_read.Array.Length - (_read.Position - _fileLength)));
+                    }
+                }
+                else
+                {
+
+                    using (FileStream write = new FileStream(toFileName, FileMode.Append))
+                    {
+                        write.Write(_read.Array, 0, _read.Array.Length);
+                    }
+                }
+
+                MainWindow.barLevel = (((double)(_read.Position / _fileLength)) * 100.0);
+            }
+            System.Windows.Forms.MessageBox.Show("Записано");
+        }
+    }
+}
